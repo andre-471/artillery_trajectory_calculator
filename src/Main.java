006c23dc388ade141ef2aee8053a6b0a6525c7c1
andre <73719598+andre-471@users.noreply.github.com>
2023-10-13 10:10:31 -0400

    new methods

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@
 // CALCULATE THE FUCKING angle of LAUNCHERR!!!
 public class Main {
     public static void main(String[] args) {
-        //
-        }
+        Projectile projectile = new Projectile(10, 10);
+        double a = projectile.angleOfProjectile(-1);
+        double b = projectile.angleOfProjectile(1);
+        System.out.println(projectile.distanceGivenAngle(a));
+        System.out.println(projectile.distanceGivenAngle(b));
     }
 }
--- src/Projectile.java
+++ src/Projectile.java
@@ -1,3 +1,32 @@
 public class Projectile {
-    public Projectile(double distance, double velocity)
+    private static final double gravity = 9.807;
+    private double distance;
+    private double velocity;
+
+    public Projectile(double distance, double velocity) {
+        this.distance = distance;
+        this.velocity = velocity;
+    }
+
+    public void setDistance(double newDistance) {
+        distance = newDistance;
+    }
+
+    // https://en.wikipedia.org/wiki/Projectile_motion#Angle_%CE%B8_required_to_hit_coordinate_(x,_y)
+    public double angleOfProjectile(int sign) {
+        double discriminant = Math.pow(Math.pow(velocity, 4) - gravity * (gravity * Math.pow(distance, 2)), 0.5);
+        if (sign < 0) {
+            // lower angle of launch
+            return Math.toRadians(Math.atan((Math.pow(velocity, 2) - discriminant) / (gravity * distance)));
+        } else {
+            // higher angle of launch
+            return Math.toRadians(Math.atan((Math.pow(velocity, 2) + discriminant) / (gravity * distance)));
+        }
+    }
+
+    public double distanceGivenAngle(double angle) {
+        return Math.pow(velocity, 2) * Math.sin(2 * Math.toDegrees(angle)) / Math.abs(gravity);
+    }
+
+
 }
